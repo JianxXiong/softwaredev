6d7d0ecbe7b6120b4c1fcbae5388807fadc1f3f5
熊健 <jianx@JiandeMacBook-Pro.local>
2024-10-31 16:56:59 +0800

    edit_element_id and edit_element_content

--- lab1/code/model/html_document.py
+++ lab1/code/model/html_document.py
@@ -4,7 +4,8 @@ from html_element import HTMLElement
 class HTMLDocument:
 
     def __init__(self, title="My Webapp") -> None:
-        self.id = ["html", "body", "title", "head"]
+        #ids为了方便判断是否存在
+        self.ids = ["html", "body", "title", "head"]
         self.html = HTMLElement("html")
         self.body = HTMLElement("body")
         self.head = HTMLElement("head")
@@ -16,9 +17,20 @@ class HTMLDocument:
 
     def set_title(self, new_title) -> None:
         self.title.set_content(new_title)
-
+    #按照元素id判断元素是否存在
+    def whether_exists_id(self, id):
+        if id in self.ids:
+            return True
+        return False
+    #判断元素是否存在
+    def whether_exists_element(self, element):
+        return self.whether_exists_id(element.id)
     
+    #通过id获取元素
     def find_element_by_id(self, element, target_id):
+            if self.whether_exists_id(target_id) is False:
+                print(f"target element with this id: {target_id} doesn`t exsist!")
+                return None
             for child in element.children:
                 if child.id == target_id:
                     return child
@@ -29,6 +41,12 @@ class HTMLDocument:
 
     #在某元素后插入元素
     def insert_after(self, target_id, new_element) -> None:
+        if self.whether_exists_element(new_element):
+            print(f"element with this id: {new_element.id} already exsists!")
+            return
+        if self.whether_exists_id(target_id) is False:
+            print(f"target element with this id: {target_id} doesn`t exsist!")
+            return
         target_element = self.find_element_by_id(self.html, target_id)
         if target_element:
             parent = target_element.parent
@@ -37,18 +55,47 @@ class HTMLDocument:
                 if index < len(parent.children) - 1:
                     parent.children.insert(index + 1, new_element)
                     new_element.set_parent = parent
+                    self.ids.append(new_element.id)
                 else:
                     parent.add_child(new_element)
+                    self.ids.append(new_element.id)
         else:
             print(f"Element with id '{target_id}' not found.")
 
     #向某元素内部添加子元素
     def add_into(self, target_id, new_element) -> None:
+        if self.whether_exists_element(new_element):
+            print(f"element with this id: {new_element.id} already exsists!")
+            return
+        if self.whether_exists_id(target_id) is False:
+            print(f"target element with this id: {target_id} doesn`t exsist!")
+            return
         target_element = self.find_element_by_id(self.html, target_id)
         if target_element:
             target_element.add_child(new_element)
+            self.ids.append(new_element.id)
         else:
             print(f"Element with id '{target_id}' not found.")
+
+    #修改元素id
+    def edit_element_id(self, target_id, new_id):
+        if self.whether_exists_id(new_id):
+            print(f"element with this id: {new_id} already exsists!")
+            return
+        if self.whether_exists_id(target_id) is False:
+            print(f"element with this id: {target_id} doesn`t exsists!")
+            return
+        target_element = self.find_element_by_id(self.html, target_id)
+        self.ids[self.ids.index(target_id)] = new_id
+        target_element.set_id(new_id)
+
+    #修改元素文本
+    def edit_element_content(self, target_id, new_content):
+        if self.whether_exists_id(target_id) is False:
+            print(f"element with this id: {target_id} doesn`t exsists!")
+            return
+        target_element = self.find_element_by_id(self.html, target_id)
+        target_element.set_content(new_content)
 
     #树的格式
     def _display_tree(self, element, level, is_first, is_last, prefix):
@@ -84,4 +131,11 @@ class HTMLDocument:
 
 if __name__ == "__main__":
     document = HTMLDocument(title="My Webapp")
+    res = document.display_indent_structure()
+    res = document.add_into("body", HTMLElement("h1", "h111111", None, "h11"))
+    res = document.add_into("body", HTMLElement("h1", "h111111", None, "h12"))
+    res = document.insert_after("h11", HTMLElement("h1", "h111111", None, "h13"))
+    res = document.insert_after("h11", HTMLElement("h1", "h111111", None, "h13"))
+    res = document.edit_element_id("h11", "h14")
+    res = document.edit_element_content("h11", "hahahahha")
     res = document.display_indent_structure()
